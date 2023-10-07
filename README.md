05868106082333eaec4be86926e9bcd4e20f5cc1
Danny Betancourt <dannybetancourt@triumvirate.dev>
2023-10-07 11:37:18 -0500

    Adjustments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Welcome! Getting started with this image is very simple. Each of the 30 installed languages has a dedicated directory with a starter file you can run. Below are the commands to execute each of these files. To run the starter code, change into the language's directory (for example, `cd /home/root/CrashCourse/Ada`) and run the relevant command. There's also a convenient script to execute all* of the `Hello, world!`s located in `/home/root` (the same directory you load into when running this image) named `sanity_check.sh`. To execute it, run `bash sanity_check.sh`.
+Welcome! Getting started with this image is very simple. Each of the 30 installed languages has a dedicated directory with a starter file you can run. Below are the commands to execute each of these files. To run the starter code, change into the language's directory (for example, `cd /home/root/CrashCourse/Ada`) and run the relevant command. There's also a convenient script to execute all* of the `Hello, world!`s located right here in this directory named `sanity_check.sh`. To execute it, run `bash sanity_check.sh`.
 
 *Erlang and Occam-π are not included in this script, so they must be run manually.
 
@@ -97,4 +97,4 @@ UIUA
 uiua run hello.ua
 
 Zig
-zig build run
+zig run hello.zig
---
+++ sanity_check.sh
@@ -0,0 +1,30 @@
+clear && originalWorkingDirectory=`pwd` && \
+echo -e "=============== Ada ===============\n\n" && cd /home/root/CrashCourse/Ada && gnatmake hello.adb > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== Agda ===============\n\n" && cd ../Agda && agda --compile hello.agda > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== BQN ===============\n\n" && cd ../BQN && BQN hello.bqn && \
+echo -e "\n\n=============== Chapel ===============\n\n" && cd ../Chapel && chpl hello.chpl -o hello > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== CPlusPlus ===============\n\n" && cd ../CPlusPlus && g++ hello.cpp -o hello -Wall -Wextra -Werror -std=c++20 > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== CSharp ===============\n\n" && cd ../CSharp && dotnet run && \
+echo -e "\n\n=============== Dart ===============\n\n" && cd ../Dart/hello && dart run && \
+echo -e "\n\n=============== Dex ===============\n\n" && cd ../../Dex && dex script hello.dx && \
+echo -e "\n\n=============== Eiffel ===============\n\n" && cd ../Eiffel && yes | ec hello.e > /dev/null 2>&1 && sudo chmod u+x hello && sudo ./hello && \
+echo -e "\n\n=============== Elixir ===============\n\n" && cd ../Elixir && elixir hello.exs && \
+echo -e "\n\n=============== Factor ===============\n\n" && cd ../Factor && factor hello.factor && \
+echo -e "\n\n=============== Fortran ===============\n\n" && cd ../Fortran && gfortran hello.f90 -o hello > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== Go ===============\n\n" && cd ../Go && go run . && \
+echo -e "\n\n=============== Haskell ===============\n\n" && cd ../Haskell && ghc -dynamic hello.hs > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== Julia ===============\n\n" && cd ../Julia && julia hello.jl && \
+echo -e "\n\n=============== Koka ===============\n\n" && cd ../Koka && koka hello.kk > /dev/null 2>&1 && .koka/v2.4.2/gcc-debug/hello && \
+echo -e "\n\n=============== Mercury ===============\n\n" && cd ../Mercury && mmc --make hello > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== Mojo ===============\n\n" && cd ../Mojo && python hello.py && \
+echo -e "\n\n=============== OCaml ===============\n\n" && cd ../Ocaml && ocaml hello.ml && \
+echo -e "\n\n=============== Perl ===============\n\n" && cd ../Perl && perl hello.pl && \
+echo -e "\n\n=============== Pharo ===============\n\n" && cd ../Pharo && pharo /home/root/pharo/Pharo.image hello.st && \
+echo -e "\n\n=============== Racket ===============\n\n" && cd ../Racket && racket hello.rkt && \
+echo -e "\n\n=============== Ruby ===============\n\n" && cd ../Ruby && ruby hello.rb && \
+echo -e "\n\n=============== Rust ===============\n\n" && cd ../Rust && rustc hello.rs > /dev/null 2>&1 && ./hello && \
+echo -e "\n\n=============== Swift ===============\n\n" && cd ../Swift && swift hello.swift && \
+echo -e "\n\n=============== TypeScript ===============\n\n" && cd ../TypeScript && bun hello.ts && \
+echo -e "\n\n=============== UIUA ===============\n\n" && cd ../UIUA && uiua run hello.ua && \
+echo -e "\n\n=============== Zig ===============\n\n" && cd ../Zig && zig run hello.zig && \
+cd $originalWorkingDirectory
